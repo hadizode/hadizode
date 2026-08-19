25ebd3a798b3d4e7c5e69053ddb39d4df2a5f4b8
hadizode <aliboicartiw@gmail.com>
2026-08-19 19:26:09 +0330

    next commit

--- backend/config/config.php
+++ backend/config/config.php
@@ -5,7 +5,7 @@
  */ 
  
 // error_reporting(E_ALL); 
-ini_set('display_errors', '1');   // نمایش خطاها برای دیباگ 
+//ini_set('display_errors', '1');   // نمایش خطاها برای دیباگ 
 // ini_set('display_startup_errors', '1'); 
 ini_set('log_errors', '1'); 
  
